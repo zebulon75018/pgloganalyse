a1cb8f0b305ca6daa2ef2ff97325cdc2732bcc23
zebulon75018 <zebulon75018@yahoo.fr>
2019-12-29 01:25:37 +0100

    add class

--- pgloganalyse.py
+++ pgloganalyse.py
@@ -1,6 +1,16 @@
+import dateparser
 
+class PaserLineLog:
+	def __init__(self, line):
+		# Parse Date ...
+		datestr = line.split("]")
+		if len(datestr)>1:
+			print(datestr[0].replace("[",""))
+			print(dateparser.parse(datestr[0].replace("[","")))
+	
 
 with open('log.txt', 'r') as reader:
 	for line in reader:
+		p = PaserLineLog(line)
 		print(line)
 		
